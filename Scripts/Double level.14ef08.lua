9b655f934a3bcf97a4635e1a36ac7db1696b5541
FVMF <frankverberne@gmail.com>
2025-01-26 22:12:54 +0100

    Fix double level tokens

--- Scripts/Double level.14ef08.lua
+++ Scripts/Double level.14ef08.lua
@@ -1,12 +1,18 @@
--- Double level Munchkin
+--Double level Munchkin
+pos = {6.26, 0.97, 7.50}
+rot = {0, 180, 0}
 
-function onLoad()
-  levelCounters = Global.getTable('levelCounters')
-  playerColors = Global.getTable('playerColors')
-  ownColor = ''
-  pos = self.getPosition()
-  rot = self.getRotation()
-
+function onLoad(script_state)
+  local state = JSON.decode(script_state)
+  if state == '' or state == nil then
+    levelCounters = Global.getTable('levelCounters')
+    playerColors = Global.getTable('playerColors')
+    ownColor = ''
+  else
+    levelCounters = {['Red'] = getObjectFromGUID(state.guids.red), ['Orange'] = getObjectFromGUID(state.guids.orange), ['Yellow'] = getObjectFromGUID(state.guids.yellow), ['Green'] = getObjectFromGUID(state.guids.green), ['Blue'] = getObjectFromGUID(state.guids.blue), ['Purple'] = getObjectFromGUID(state.guids.purple)}
+    playerColors = state.playerColors
+    ownColor = state.ownColor
+  end
 end --onLoad
 
 function onPlayerTurn()
@@ -15,7 +21,6 @@ function onPlayerTurn()
   ownColor = ''
   self.setPositionSmooth(pos, false, false)
   self.setRotationSmooth(rot, false, false)
-
 end --onPlayerTurn
 
 function has_value (tab, val)
@@ -25,7 +30,6 @@ function has_value (tab, val)
     end
   end
   return false
-
 end --has_value
 
 function onPickUp(playerColor)
@@ -35,5 +39,10 @@ function onPickUp(playerColor)
     currentLevel = levelCounters[playerColor].getValue()
     self.setDescription(currentLevel)
   end
-
 end --onPickUp
+
+function onSave()
+  local state = {guids = 
+  {red = levelCounters['Red'].guid, orange = levelCounters['Orange'].guid, yellow = levelCounters['Yellow'].guid, green = levelCounters['Green'].guid, blue = levelCounters['Blue'].guid, purple = levelCounters['Purple'].guid}, playerColors = playerColors, ownColor = ownColor}
+  return JSON.encode(state)
+end --onSave
--- Scripts/Double level.2bc284.lua
+++ Scripts/Double level.2bc284.lua
@@ -1,23 +1,26 @@
 --Double level Monster
+pos = {-6.73, 0.97, 7.50}
+rot = {0, 180, 0}
 
-function onLoad()
-  zoneToUpdate = Global.getTable('equipmentBonusZones')['Monster Support']
-  previousValue = 0
-  pos = self.getPosition()
-  rot = self.getRotation()
-
-end --function onLoad
+function onLoad(script_state)
+  local state = JSON.decode(script_state)
+  if state == '' or state == nil then
+    zoneToUpdate = Global.getTable('equipmentBonusZones')['Monster Support']
+    previousValue = 0
+  else
+    zoneToUpdate = getObjectFromGUID(state.guids.zoneToUpdate)
+    previousValue = state.previousValue
+  end
+end --onLoad
 
 function onPlayerTurn()
   self.setPositionSmooth(pos, false, false)
   self.setRotationSmooth(rot, false, false)
   previousValue = 0
-
 end --function onPlayerTurn
 
 function onPickUp()
   self.setDescription('')
-
 end --function onPickUp
 
 function onDrop()
@@ -31,7 +34,7 @@ function onDrop()
               origin       = physicsPosition,
               direction    = {0,-1,0},
               type         = 2,
-              size         = {radius,radius,radius},
+              size         = {radius,radius,radius},
               max_distance = 0,
               debug        = true,
         })
@@ -40,5 +43,9 @@ function onDrop()
       end
     end
   end
-
 end --function onDrop
+
+function onSave()
+  local state = {guids = {zoneToUpdate = zoneToUpdate.guid}, previousValue = previousValue}
+  return JSON.encode(state)
+end --onSave
