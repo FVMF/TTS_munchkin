ca8f61e7466192527fd9eadc339121e76817f35c
FVMF <frankverberne@gmail.com>
2025-01-26 23:50:01 +0100

    Revert "Mate token onSave & onLoad"
    This reverts commit 2d202f56a8e98f6861c238ceecf1491fcda950c6.

--- Scripts/Mate.5bb3d2.lua
+++ Scripts/Mate.5bb3d2.lua
@@ -1,31 +1,20 @@
 -- Mate
 VISIBLE_ONCE = false
-castVisible = true
 RADIUS = 4
 
-function onLoad(script_state)
-   local state = JSON.decode(script_state) 
-   if state == '' or state == nil then
-      zoneToUpdate = Global.getTable('equipmentBonusZones')['Monster Support']
-      currentValue = 0
-      pos = {-5.10, 0.97, 7.51}
-      rot = {0, 180, 0}
-      return JSON.encode(state)
-   else
-      zoneToUpdate = getObjectFromGUID(state.zoneToUpdateGUID)
-      currentValue = state.currentValue
-      pos = state.pos
-      rot = state.rot 
-      objectInCorrectZone = state.objectInCorrectZone
-      physicsPosition = state.physicsPosition
-   end
+function onLoad()
+   zoneToUpdate = Global.getTable('equipmentBonusZones')['Monster Support']
+   previousValue = 0
+   castVisible = true
+   pos = self.getPosition()
+   rot = self.getRotation()
 end --function onLoad
 
 function onPlayerTurn()
    self.setDescription(0)
-   self.setPositionSmooth({-5.10, 0.97, 7.51}, false, false)
-   self.setRotationSmooth({0, 180, 0}, false, false)
-   currentValue = 0
+   self.setPositionSmooth(pos, false, false)
+   self.setRotationSmooth(rot, false, false)
+   previousValue = 0
    objectInCorrectZone = false
 end --function onPlayerTurn
 
@@ -56,12 +45,10 @@ function getValueOfCardsInArea()
    if objectInCorrectZone then
       valueOfCards = 0
       hitlist = Physics.cast({
-           origin       = physicsPosition,
-
+           origin       = physicsPosition,
            direction    = {0,-1,0},
            type         = 2,
-           size         = {RADIUS, RADIUS, RADIUS},
-
+           size         = {RADIUS, RADIUS, RADIUS},
            max_distance = 0,
            debug        = castVisible,
            })
@@ -69,24 +56,18 @@ function getValueOfCardsInArea()
          castVisible = false
       end
       for _, hit in ipairs(hitlist) do
-         if hit.hit_object != self and hit.hit_object.getDescription() ~= '' then
+         if hit.hit_object != self then
             objectDescription = hit.hit_object.getDescription()
-            if Global.call('validDescription', objectDescription) then
-               valueOfCards = valueOfCards + tonumber(objectDescription)
-            end
          end 
+         if Global.call('validDescription', objectDescription) then
+            valueOfCards = valueOfCards + tonumber(objectDescription)
+         end
       end
    end
-   if currentValue ~= valueOfCards then
-      currentValue = valueOfCards
+   if currentValue ~= previousValue then
       self.setDescription(valueOfCards)
       Global.call('updateZoneCounter', zoneToUpdate) 
    end
 end --function getValueOfCards
-
-function onSave()
-   local state = {zoneToUpdateGUID = zoneToUpdate.guid, currentValue = currentValue, pos = self.getPosition(), rot = self.getRotation(), objectInCorrectZone = objectInCorrectZone, physicsPosition = physicsPosition}
-   return JSON.encode(state)
-end --function onSave
 
 Wait.time(getValueOfCardsInArea, 0.5, -1)
